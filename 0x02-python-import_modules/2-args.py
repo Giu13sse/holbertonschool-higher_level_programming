f3899c633f54057685f5114189ffa60dd18ff112
giu13sse <giussedipietro@gmail.com>
2022-05-07 14:33:32 -0700

    f

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import sys as s
-
 if __name__ == "__main__":
-    if (len(s.argv) is 1):
+    from sys import argv
+    if (len(argv) == 1):
         print("0 arguments.")
+    elif (len(argv) == 2):
+        print("1 argument:")
+        print(f"1: {argv[1]}")
     else:
-        if (len(s.argv) is 2):
-            print("{:d} argument:".format(len(s.argv) - 1))
-            print("1: {}".format(s.argv[1]))
-        else:
-            print("{:d} arguments:".format(len(s.argv) - 1))
-            for word, num in zip(s.argv, range(0, len(s.argv))):
-                if (num is not 0):
-                    print("{:d}: {}".format(num, word))
+        print(f"{len(argv) - 1} arguments:")
+        for i, j in enumerate(argv):
+            if (i != 0):
+                print(f"{i}: {j}")
